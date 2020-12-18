1ee057dccfc51d37486d9f5bb48f53837df76b5d
bryan bautista <bryan.bautista@tecsup.edu.pe>
2020-12-17 22:37:57 -0500

    cambio2

--- resources/views/productos/form.blade.php
+++ resources/views/productos/form.blade.php
@@ -1,22 +1,25 @@
 
+<form>
+    <div class="form-group">
+        <label for="Nombre">{{'Nombres'}}</label>
+        <input class="form-control" type="text" name="Nombre" id="Nombre" value="{{ isset($producto->Nombre)?$producto->Nombre:''}}">
+    </div>
+    <div class="form-group">
+        <label for="Categoria">{{'Apellidos'}}</label>
+        <input class="form-control" type="text" name="Categoria" id="Categoria" value="{{ isset($producto->Categoria)?$producto->Categoria:''}}">
+    </div>
+    <div class="form-group">
+        <label for="Cantidad">{{'Edad'}}</label>
+        <input class="form-control" type="text" name="Cantidad" id="Cantidad" value="{{ isset($producto->Cantidad)?$producto->Cantidad:''}}">
+    </div>
+    <div class="form-group">       
+        <label for="Precio">{{'Celular'}}</label>
+        <input class="form-control" type="text" name="Precio" id="Precio" value="{{ isset($producto->Precio)?$producto->Precio:''}}">       
+    </div>
+    <div class="form-group">
+        <label for="Foto">{{'Foto'}}</label>   
+    </div>
 
-<label for="Nombre">{{'Nombres'}}</label>
-<input type="text" name="Nombre" id="Nombre" value="{{ isset($producto->Nombre)?$producto->Nombre:''}}">
-</br>
-
-<label for="Categoria">{{'Apellidos'}}</label>
-<input type="text" name="Categoria" id="Categoria" value="{{ isset($producto->Categoria)?$producto->Categoria:''}}">
-</br>
-
-<label for="Cantidad">{{'Edad'}}</label>
-<input type="text" name="Cantidad" id="Cantidad" value="{{ isset($producto->Cantidad)?$producto->Cantidad:''}}">
-</br>
-
-<label for="Precio">{{'Celular'}}</label>
-<input type="text" name="Precio" id="Precio" value="{{ isset($producto->Precio)?$producto->Precio:''}}">
-</br>
-
-<label for="Foto">{{'Foto'}}</label>
 @if(isset($producto))
 <br/>
 <img src="{{ asset('storage').'/'. $producto->Foto}}" class="img-thumbnail img-fluid" alt="" width="100">
@@ -25,6 +28,6 @@
 <input type="file" name="Foto" id="Foto" value="">
 </br>
 
-<input type="submit" value="{{ $Modo=='crear' ? 'Agregar' :'Modificar'}}">
+<input class="form-control"  type="submit" value="{{ $Modo=='crear' ? 'Agregar' :'Modificar'}}">
 
-<a href="{{url('productos')}}">Regresar</a>
+<a class ="btn btn-danger"href="{{url('productos')}}">Regresar</a>
