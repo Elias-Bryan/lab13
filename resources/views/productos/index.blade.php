0f15361f4b0351514f2ba4de001b95dfaed59de0
bryan bautista <bryan.bautista@tecsup.edu.pe>
2020-12-17 22:13:21 -0500

    cambio

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -5,7 +5,7 @@
 @endif
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<a href="{{url('productos/create')}}" class="btn btn-success">Registrar Producto</a>
+<a href="{{url('productos/create')}}" class="btn btn-success">Registrar Contacto</a>
 <br/>
 <br/>
 
